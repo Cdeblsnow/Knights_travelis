8f8f71ce733d82f6a3d0d88e66a66450efd84826
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-06 12:17:38 -0500

    Update build_board(working)

--- lib/knight.rb
+++ lib/knight.rb
@@ -8,12 +8,19 @@ class Knight
   end
 
   def build_board(x = 0, y = 0)
+    if y >= 8
+      return hash
+    elsif x >= 8
+      return hash
+    end
+
     hash = { [x, y] => [] }
     hash[[x, y]].push([x, y + 1]) if y + 1 <= 7 # add top
     hash[[x, y]].push([x + 1, y]) if x + 1 <= 7 # add right
     hash[[x, y]].push([x, y - 1]) if y - 1 >= 0 # add bottom
     hash[[x, y]].push([x - 1, y]) if x - 1 >= 0 # add left
-
-    @board << hash
+    build_board(x, y + 1)
+    build_board(x + 1, y)
+    @board << hash unless @board.include?(hash)
   end
 end
