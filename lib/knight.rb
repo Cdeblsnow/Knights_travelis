7b769e88ff7a55f0daa115156e4bb9b19bcc43bd
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-08 03:12:59 -0500

    Update on finding shortest distance

--- lib/knight.rb
+++ lib/knight.rb
@@ -7,30 +7,32 @@ class Knight
     @moves = []
   end
 
-  def move(start, final, pos = nil)
+  def move(start, final)
     return puts "Invalid input" if start[0] > 7 || start[1] > 7
     return puts "Invalid input" if final[0] > 7 || final[1] > 7
 
     arr = [start]
+    i = 0
+    distance = []
     visited = []
-    while pos != final
+    while arr[0] != final
       break if arr[0].nil?
+      break if arr.include?(final)
 
       arr <<  [arr[0][0] + 1, arr[0][1] + 2] if arr[0][0] + 1 <= 7 && arr[0][1] + 2 <= 7 # top_right
-      arr <<  [arr[0][0] - 1, arr[0][1] + 2] if arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 # top_left
       arr <<  [arr[0][0] + 2, arr[0][1] + 1] if arr[0][0] + 2 <= 7 && arr[0][1] + 1 <= 7 # right_up
       arr <<  [arr[0][0] + 2, arr[0][1] - 1] if arr[0][0] + 2 <= 7 && arr[0][1] - 1 >= 0 # right_down
       arr <<  [arr[0][0] + 1, arr[0][1] - 2] if arr[0][0] + 1 <= 7 && arr[0][1] - 2 >= 0 # bottom_right
       arr <<  [arr[0][0] - 1, arr[0][1] - 2] if arr[0][0] - 1 >= 0 && arr[0][1] - 2 >= 0 # bottom_left
-      arr <<  [arr[0][0] - 2, arr[0][1] + 1] if arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 # left_up
       arr <<  [arr[0][0] - 2, arr[0][1] - 1] if arr[0][0] - 2 >= 0 && arr[0][1] - 1 >= 0 # left_down
-      pos = arr[0]
+      arr <<  [arr[0][0] - 2, arr[0][1] + 1] if arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 # left_up
+      arr <<  [arr[0][0] - 1, arr[0][1] + 2] if arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 # top_left
+
+      distance << [arr[0], i] unless visited.include?(arr[0])
       visited << arr[0]
+      i += 1
       arr = arr.drop(1)
     end
-    p visited
-  end
-
-  def shortest_path(start, final, visited)
+    p distance
   end
 end
