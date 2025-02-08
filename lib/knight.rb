327ec7d99e151c2f24e3d0727d52ca307dd73a6f
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-08 02:29:03 -0500

    Move method is working, but no shortest path

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,37 +1,36 @@
+require "rubocop"
 class Knight
   attr_accessor :board
 
   def initialize
     @board = []
     @moves = []
-    build_board
   end
 
-  def move(start, final, position = nil)
+  def move(start, final, pos = nil)
     return puts "Invalid input" if start[0] > 7 || start[1] > 7
     return puts "Invalid input" if final[0] > 7 || final[1] > 7
 
-    m = { moves: [] }
-    while position != final
+    arr = [start]
+    visited = []
+    while pos != final
+      break if arr[0].nil?
+
+      arr <<  [arr[0][0] + 1, arr[0][1] + 2] if arr[0][0] + 1 <= 7 && arr[0][1] + 2 <= 7 # top_right
+      arr <<  [arr[0][0] - 1, arr[0][1] + 2] if arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 # top_left
+      arr <<  [arr[0][0] + 2, arr[0][1] + 1] if arr[0][0] + 2 <= 7 && arr[0][1] + 1 <= 7 # right_up
+      arr <<  [arr[0][0] + 2, arr[0][1] - 1] if arr[0][0] + 2 <= 7 && arr[0][1] - 1 >= 0 # right_down
+      arr <<  [arr[0][0] + 1, arr[0][1] - 2] if arr[0][0] + 1 <= 7 && arr[0][1] - 2 >= 0 # bottom_right
+      arr <<  [arr[0][0] - 1, arr[0][1] - 2] if arr[0][0] - 1 >= 0 && arr[0][1] - 2 >= 0 # bottom_left
+      arr <<  [arr[0][0] - 2, arr[0][1] + 1] if arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 # left_up
+      arr <<  [arr[0][0] - 2, arr[0][1] - 1] if arr[0][0] - 2 >= 0 && arr[0][1] - 1 >= 0 # left_down
+      pos = arr[0]
+      visited << arr[0]
+      arr = arr.drop(1)
     end
+    p visited
   end
 
-  def knight_lis_of_moves(x = 0, y = 0)
-    if y >= 2
-      return hash
-    elsif x >= 2 # rubocop:disable Lint/DuplicateBranch
-      return hash
-    end
-
-    hash_y = { y: [] }
-    hash_y[:y].push([y + 2]) if y + 2 <= 7 # add top
-    hash_y[:y].push([y - 2]) if y - 2 >= 0 # add bottom
-    # each top and bottom have right and left x value
-    hash_x = { x: [] }
-    hash_x[:x].push(x - 1) if x - 1 >= 0 # left
-    hash_x[:x].push(x + 1) if x + 1 <= 7 # right
-    [x + 1,y + 2 ],[x-1,y+2],[x+1,y-2],[x-1,y-2],[x+2,y+1],[x+2,y-1],[x-2,y+1],[x-2,y-1]
-
-    @board << hash unless @board.include?(hash)
+  def shortest_path(start, final, visited)
   end
 end
