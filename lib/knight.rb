9a75aeade293db0109f5eeae9b0ffe883abee465
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-11 22:40:34 -0500

    Update move method

--- lib/knight.rb
+++ lib/knight.rb
@@ -17,41 +17,43 @@ class Knight
     while position != final
       break if arr[0].nil?
 
-      current = next_vertex(arr[-1], rejected)
+      current = next_vertex(arr[-1], rejected, visited)
+
+      current = visited[-1] if current.nil?
 
       if current[0] > final[0] || current[1] > final[1] # check if it is out of range
         arr.unshift(current)
         rejected << current
       else
         arr << current
+        visited << current
       end
 
-      visited << arr[0]
       position = arr[-1]
       break if position == final
 
       arr = arr.drop(1)
     end
-    arr.unshift(start)
-    p arr
+
+    p visited
   end
 
-  def next_vertex(arr, rejected, hold = [])
-    if arr[0] + 1 <= 7 && arr[1] + 2 <= 7 && rejected.include?([arr[0] + 1, arr[1] + 2]) == false # top_right
+  def next_vertex(arr, rejected, visited, hold = [])
+    if (arr[0] + 1 <= 7 && arr[1] + 2 <= 7) && rejected.include?([arr[0] + 1, arr[1] + 2]) == false && visited.include?([arr[0] + 1, arr[1] + 2]) == false # top_right
       hold << [arr[0] + 1, arr[1] + 2]
-    elsif arr[0] + 2 <= 7 && arr[1] + 1 <= 7 && rejected.include?([arr[0] + 2, arr[1] + 1]) == false # right_up
+    elsif (arr[0] + 2 <= 7 && arr[1] + 1 <= 7) && rejected.include?([arr[0] + 2, arr[1] + 1]) == false && visited.include?([arr[0] + 2, arr[1] + 1]) == false # right_up
       hold << [arr[0] + 2, arr[1] + 1]
-    elsif arr[0] + 2 <= 7 && arr[1] - 1 >= 0 && rejected.include?([arr[0] + 2, arr[1] - 1]) == false # right_down
+    elsif (arr[0] + 2 <= 7 && arr[1] - 1 >= 0) && rejected.include?([arr[0] + 2, arr[1] - 1]) == false && visited.include?([arr[0] + 2, arr[1] - 1]) == false # right_down
       hold <<  [arr[0] + 2, arr[1] - 1]
-    elsif arr[0] + 1 <= 7 && arr[1] - 2 >= 0 && rejected.include?([arr[0] + 1, arr[1] - 2]) == false # bottom_right
+    elsif (arr[0] + 1 <= 7 && arr[1] - 2 >= 0) && rejected.include?([arr[0] + 1, arr[1] - 2]) == false && visited.include?([arr[0] + 1, arr[1] - 2]) == false # bottom_right
       hold <<  [arr[0] + 1, arr[1] - 2]
-    elsif arr[0] - 1 >= 0 && arr[1] - 2 >= 0 && rejected.include?([arr[0] - 1, arr[1] - 2]) == false # bottom_left
+    elsif (arr[0] - 1 >= 0 && arr[1] - 2 >= 0) && rejected.include?([arr[0] - 1, arr[1] - 2]) == false && visited.include?([arr[0] - 1, arr[1] - 2]) == false # bottom_left
       hold <<  [arr[0] - 1, arr[1] - 2]
-    elsif arr[0] - 2 >= 0 && arr[1] - 1 >= 0 && rejected.include?([arr[0] - 2, arr[1] - 1]) == false # left_down
+    elsif (arr[0] - 2 >= 0 && arr[1] - 1 >= 0) && rejected.include?([arr[0] - 2, arr[1] - 1]) == false && visited.include?([arr[0] - 2, arr[1] - 1]) == false # left_down
       hold <<  [arr[0] - 2, arr[1] - 1]
-    elsif arr[0] - 2 >= 0 && arr[1] + 1 <= 7 && rejected.include?([arr[0] - 2, arr[1] + 1]) == false # left_up
+    elsif (arr[0] - 2 >= 0 && arr[1] + 1 <= 7) && rejected.include?([arr[0] - 2, arr[1] + 1]) == false && visited.include?([arr[0] - 2, arr[1] + 1]) == false # left_up
       hold <<  [arr[0] - 2, arr[1] + 1]
-    elsif arr[0] - 1 >= 0 && arr[1] + 2 <= 7 && rejected.include?([arr[0] - 1, arr[1] + 2]) == false # top_left
+    elsif (arr[0] - 1 >= 0 && arr[1] + 2 <= 7) && rejected.include?([arr[0] - 1, arr[1] + 2]) == false && visited.include?([arr[0] - 1, arr[1] + 2]) == false # top_left
       hold <<  [arr[0] - 1, arr[1] + 2]
     end
     hold[-1]
