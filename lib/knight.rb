65351ecf41fd1364d973aeabaea92aa89cf17cf4
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-11 22:03:11 -0500

    Work on move method

--- lib/knight.rb
+++ lib/knight.rb
@@ -12,38 +12,45 @@ class Knight
     return puts "Invalid input" if final[0] > 7 || final[1] > 7
 
     arr = [start]
-    visited = [start]
+    visited = []
     rejected = []
     while position != final
       break if arr[0].nil?
 
-      visited << arr[1]
+      current = next_vertex(arr[-1], rejected)
 
+      if current[0] > final[0] || current[1] > final[1] # check if it is out of range
+        arr.unshift(current)
+        rejected << current
+      else
+        arr << current
+      end
+
+      visited << arr[0]
       position = arr[0]
-
       arr = arr.drop(1)
     end
     p visited
   end
 
-  def next_vertex(start, final, rejected,  arr = [])
-    if arr[0][0] + 1 <= 7 && arr[0][1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_right
-      arr << [arr[0][0] + 1, arr[0][1] + 2]
-    elsif arr[0][0] + 2 <= 7 && arr[0][1] + 1 <= 7 && rejected.include?(arr[0]) == false # right_up
-      arr << [arr[0][0] + 2, arr[0][1] + 1]
-    elsif arr[0][0] + 2 <= 7 && arr[0][1] - 1 >= 0 && rejected.include?(arr[0]) == false # right_down
-      arr <<  [arr[0][0] + 2, arr[0][1] - 1]
-    elsif arr[0][0] + 1 <= 7 && arr[0][1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_right
-      arr <<  [arr[0][0] + 1, arr[0][1] - 2]
-    elsif arr[0][0] - 1 >= 0 && arr[0][1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_left
-      arr <<  [arr[0][0] - 1, arr[0][1] - 2]
-    elsif arr[0][0] - 2 >= 0 && arr[0][1] - 1 >= 0 && rejected.include?(arr[0]) == false # left_down
-      arr <<  [arr[0][0] - 2, arr[0][1] - 1]
-    elsif arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 && rejected.include?(arr[0]) == false # left_up
-      arr <<  [arr[0][0] - 2, arr[0][1] + 1]
-    elsif arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_left
-      arr <<  [arr[0][0] - 1, arr[0][1] + 2]
+  def next_vertex(arr, rejected, hold = [])
+    if arr[0] + 1 <= 7 && arr[1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_right
+      hold << [arr[0] + 1, arr[1] + 2]
+    elsif arr[0] + 2 <= 7 && arr[1] + 1 <= 7 && rejected.include?(arr[0]) == false # right_up
+      hold << [arr[0] + 2, arr[1] + 1]
+    elsif arr[0] + 2 <= 7 && arr[1] - 1 >= 0 && rejected.include?(arr[0]) == false # right_down
+      hold <<  [arr[0] + 2, arr[1] - 1]
+    elsif arr[0] + 1 <= 7 && arr[1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_right
+      hold <<  [arr[0] + 1, arr[1] - 2]
+    elsif arr[0] - 1 >= 0 && arr[1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_left
+      hold <<  [arr[0] - 1, arr[1] - 2]
+    elsif arr[0] - 2 >= 0 && arr[1] - 1 >= 0 && rejected.include?(arr[0]) == false # left_down
+      hold <<  [arr[0] - 2, arr[1] - 1]
+    elsif arr[0] - 2 >= 0 && arr[1] + 1 <= 7 && rejected.include?(arr[0]) == false # left_up
+      hold <<  [arr[0] - 2, arr[1] + 1]
+    elsif arr[0] - 1 >= 0 && arr[1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_left
+      hold <<  [arr[0] - 1, arr[1] + 2]
     end
-    arr
+    hold[-1]
   end
 end
