6d958b2689855d25ea974dba05eda5025b2c1ded
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-02-11 21:13:27 -0500

    Work on move method

--- lib/knight.rb
+++ lib/knight.rb
@@ -7,32 +7,43 @@ class Knight
     @moves = []
   end
 
-  def move(start, final)
+  def move(start, final, position = nil)
     return puts "Invalid input" if start[0] > 7 || start[1] > 7
     return puts "Invalid input" if final[0] > 7 || final[1] > 7
 
     arr = [start]
-    i = 0
-    distance = []
-    visited = []
-    while arr[0] != final
+    visited = [start]
+    rejected = []
+    while position != final
       break if arr[0].nil?
-      break if arr.include?(final)
 
-      arr <<  [arr[0][0] + 1, arr[0][1] + 2] if arr[0][0] + 1 <= 7 && arr[0][1] + 2 <= 7 # top_right
-      arr <<  [arr[0][0] + 2, arr[0][1] + 1] if arr[0][0] + 2 <= 7 && arr[0][1] + 1 <= 7 # right_up
-      arr <<  [arr[0][0] + 2, arr[0][1] - 1] if arr[0][0] + 2 <= 7 && arr[0][1] - 1 >= 0 # right_down
-      arr <<  [arr[0][0] + 1, arr[0][1] - 2] if arr[0][0] + 1 <= 7 && arr[0][1] - 2 >= 0 # bottom_right
-      arr <<  [arr[0][0] - 1, arr[0][1] - 2] if arr[0][0] - 1 >= 0 && arr[0][1] - 2 >= 0 # bottom_left
-      arr <<  [arr[0][0] - 2, arr[0][1] - 1] if arr[0][0] - 2 >= 0 && arr[0][1] - 1 >= 0 # left_down
-      arr <<  [arr[0][0] - 2, arr[0][1] + 1] if arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 # left_up
-      arr <<  [arr[0][0] - 1, arr[0][1] + 2] if arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 # top_left
+      visited << arr[1]
 
-      distance << [arr[0], i] unless visited.include?(arr[0])
-      visited << arr[0]
-      i += 1
+      position = arr[0]
+
       arr = arr.drop(1)
     end
-    p distance
+    p visited
+  end
+
+  def next_vertex(start, final, rejected,  arr = [])
+    if arr[0][0] + 1 <= 7 && arr[0][1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_right
+      arr << [arr[0][0] + 1, arr[0][1] + 2]
+    elsif arr[0][0] + 2 <= 7 && arr[0][1] + 1 <= 7 && rejected.include?(arr[0]) == false # right_up
+      arr << [arr[0][0] + 2, arr[0][1] + 1]
+    elsif arr[0][0] + 2 <= 7 && arr[0][1] - 1 >= 0 && rejected.include?(arr[0]) == false # right_down
+      arr <<  [arr[0][0] + 2, arr[0][1] - 1]
+    elsif arr[0][0] + 1 <= 7 && arr[0][1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_right
+      arr <<  [arr[0][0] + 1, arr[0][1] - 2]
+    elsif arr[0][0] - 1 >= 0 && arr[0][1] - 2 >= 0 && rejected.include?(arr[0]) == false # bottom_left
+      arr <<  [arr[0][0] - 1, arr[0][1] - 2]
+    elsif arr[0][0] - 2 >= 0 && arr[0][1] - 1 >= 0 && rejected.include?(arr[0]) == false # left_down
+      arr <<  [arr[0][0] - 2, arr[0][1] - 1]
+    elsif arr[0][0] - 2 >= 0 && arr[0][1] + 1 <= 7 && rejected.include?(arr[0]) == false # left_up
+      arr <<  [arr[0][0] - 2, arr[0][1] + 1]
+    elsif arr[0][0] - 1 >= 0 && arr[0][1] + 2 <= 7 && rejected.include?(arr[0]) == false # top_left
+      arr <<  [arr[0][0] - 1, arr[0][1] + 2]
+    end
+    arr
   end
 end
